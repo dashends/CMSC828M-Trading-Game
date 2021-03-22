b6d62d7013b90ad740b759c68eac9667b528a6c0
dashends <zweb12222@gmail.com>
2021-03-22 14:18:47 -0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CMSC828M Trading Game
  
+1. Implement the game environment. We can use OpenAI Gym following this trading game example: https://towardsdatascience.com/creating-a-custom-openai-gym-environment-for-stock-trading-be532be3910e
---
+++ env/TradingGameEnv.py
@@ -0,0 +1,15 @@
+import gym
+from gym import spaces
+import numpy as np
+import random
+
+class TradingGameEnv(gym.Env):
+	"""A trading game environment for OpenAI gym"""
+	
+	def __init__(self):
+		
+		
+	def _next_observation(self):
+		return np.array([])
+	
+	
